30c7ab296df7c4e2147396ed4f21ccfc664195ef
nycolemendonca-acs <nycolemendonca.acs@gmail.com>
2023-06-26 16:37:11 -0300

    unificando apis

--- passengers-api/src/main/java/app/car/passengersapi/domain/PassengerRepository.java
+++
@@ -1,5 +0,0 @@
-package app.car.passengersapi.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {}
